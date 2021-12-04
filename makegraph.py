8948c4def9942e3f8dd8c9b4914d9cb4e5871564
claiire38 <dickcl@student.ethz.ch>
2021-12-04 09:32:03 -0800

    cd

--- makegraph.py
+++ makegraph.py
@@ -47,8 +47,7 @@ def make_graph(size,distribution):
     
     #graph creation
     #first statement gives you a fully randomized graph while the second one gives you a REGULAR graph with all nodes having 5 degrees
-    if 1 ==0:
-        size = 100
+    if 0 ==1:
         g = Graph(directed=False)
     
         # insert random vertices (nodes)
@@ -58,7 +57,7 @@ def make_graph(size,distribution):
         for s,t in zip(randint(0, size, 2*size), randint(0, size, 2*size)):
             g.add_edge(g.vertex(s), g.vertex(t))
     else:
-         g=graph_tool.generation.price_network(size, m=5, c=None, gamma=1, directed=False,seed_graph=None)
+         g=graph_tool.generation.price_network(size, m=3, c=None, gamma=1, directed=False,seed_graph=None)
     
     #definition of vertex properties
     global S          # White color
